167ebc10c21a41c118e85f3a5e158e6d1e5f1a53
Abhinav Garg <abhigarg1921@gmail.com>
2025-08-13 01:54:13 +0530

    Add all project files

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+let items = ["Buy Food", "Cook Food" , "Eat food"]
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("Public"))
+
+app.get('/', function(req, res) {
+  
+    let today = new Date()
+    let options = {weekday: "long", day: "numeric", month: "long"}
+    let day = today.toLocaleDateString("en-us", options)
+
+    res.render("list", {kindOfDay: day, addItems: items})
+
+});
+
+app.post("/", function(req,res){
+    let item = req.body.newItem
+    items.push(item)
+
+    res.redirect("/")
+
+})
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
